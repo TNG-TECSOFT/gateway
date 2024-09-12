56469ccd5daa4dcf3ac58d54677578cc30e37e6a
matiaslgarcia <matiasgarcia444@gmail.com>
2024-09-12 11:55:35 -0300

    custom filter

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Inject,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateBillingDto } from './dto/create-billing.dto';
 import { firstValueFrom } from 'rxjs';
@@ -14,15 +7,21 @@ import { firstValueFrom } from 'rxjs';
 export class BillingController {
   constructor(
     @Inject('BILLING_SERVICE')
-    private readonly client: ClientProxy) {}
+    private readonly client: ClientProxy,
+  ) {}
 
   @Get('/client/:clientId')
   async getClientData(@Param('clientId') clientId: string) {
     try {
-      const response = await firstValueFrom(this.client.send<string, string>('getClientData', clientId));
+      const response = await firstValueFrom(
+        this.client.send<string, string>('getClientData', clientId),
+      );
       return response;
     } catch (error) {
-      console.error('Error communicating with the microservice:', error.message);
+      console.error(
+        'Error communicating with the microservice:',
+        error.message,
+      );
       throw new Error('Failed to retrieve client data');
     }
   }
@@ -30,10 +29,15 @@ export class BillingController {
   @Get('/product/:sku')
   async getProductData(@Param('sku') sku: string) {
     try {
-      const response = await firstValueFrom(this.client.send<string, string>('getProductData', sku));
+      const response = await firstValueFrom(
+        this.client.send<string, string>('getProductData', sku),
+      );
       return response;
     } catch (error) {
-      console.error('Error communicating with the microservice:', error.message);
+      console.error(
+        'Error communicating with the microservice:',
+        error.message,
+      );
       throw new Error('Failed to retrieve product data');
     }
   }
@@ -41,10 +45,15 @@ export class BillingController {
   @Post('/order')
   async createOrder(@Body() createOrderDto: CreateBillingDto) {
     try {
-      const response = await firstValueFrom(this.client.send('createOrder', createOrderDto));
+      const response = await firstValueFrom(
+        this.client.send('createOrder', createOrderDto),
+      );
       return response;
     } catch (error) {
-      console.error('Error communicating with the microservice:', error.message);
+      console.error(
+        'Error communicating with the microservice:',
+        error.message,
+      );
       throw new Error('Failed to create order');
     }
   }
