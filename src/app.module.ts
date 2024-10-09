851eeea5089ffb239e441a7b1ecc0216835273f6
Diego Ferrando <dferrando@tecnosoftware.com>
2024-10-09 11:40:40 -0300

    Added BillingModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TangoModule } from './tango/tango.module';
 import { AppController } from './app.controller';
+import { BillingModule } from './billing/billing.module';
 
 @Module({
   imports: [
-    TangoModule
+    TangoModule,
+    BillingModule,
   ],
   controllers: [AppController],
   providers: [],
